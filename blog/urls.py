e03774bc90284b626cbbb08e441ac2d200d287ab
Farov Amir <farov_a@iuca.kg>
2025-10-14 22:03:47 +0600

    add blog/urls.py, views.py-edit_player

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.player_list, name='player_list'),
     path('create/', views.create_player, name='create_player'),
+    path('edit/<int:pk>/', views.edit_player, name='edit_player'),
     ]
--- blog/views.py
+++ blog/views.py
@@ -20,3 +20,23 @@ def create_player(request):
     else:
         form = PlayerForm()
     return render(request, 'blog/create_player.html', {'form': form})
+
+# Редактировать игрока
+def edit_player(request, pk):
+    player = get_object_or_404(Player, pk=pk)
+    message = ''
+    player_id = request.session.get('player_id')
+    if not player_id:
+        return redirect('login')
+    # Проверка, принадлежит ли редактируемый профиль текущему пользователю
+    if request.session['player_id'] != player.id:
+        message = 'У вас нет прав для редактирования этого профиля'
+        return render(request, 'blog/edit_player.html', {'player': player, 'message': message})
+    if request.method == 'POST':
+        form = PlayerForm(request.POST, instance=player)
+        if form.is_valid():
+            form.save()
+            return redirect('player_list')
+    else:
+        form = PlayerForm(instance=player)
+    return render(request, 'blog/edit_player.html', {'form': form, 'player': player, 'message': message})
